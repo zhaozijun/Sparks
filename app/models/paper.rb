ae22f800196db7c83c10d6fd4fc6baf2f27de1e4
赵紫君 <zhaozijun@hntvlan569.3282.wlan.wireless-resnet.upenn.edu>
2015-04-11 10:40:45 -0400

    author

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: papers
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Paper < ActiveRecord::Base
   attr_accessible :title
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SPARKs::Application.routes.draw do
 
   devise_for :authors
 
+  root :to => redirect('/papers')
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
