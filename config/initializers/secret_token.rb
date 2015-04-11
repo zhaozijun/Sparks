3785ab25027a9e08d6168967638e0087449181bb
赵紫君 <zhaozijun@hntvlan569.3282.wlan.wireless-resnet.upenn.edu>
2015-04-10 21:14:23 -0400

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SPARKs::Application.config.secret_token = '1988c551defc5d85aca5dbe56227d1c68eb160c524d0446acfe7f7a4888619057522a9d03b9afe399ce5d9451f6ea764ff747d521c44cbbd21524a969bc9af6b'
