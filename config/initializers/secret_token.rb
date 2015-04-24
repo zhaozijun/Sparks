48544cac9e1e0c11eb129f0a78855c689c30b27d
赵紫君 <zhaozijun@seas1407.wireless-pennnet.upenn.edu>
2015-04-24 14:46:32 -0400

    button style

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SPARKs::Application.config.secret_token = '1988c551defc5d85aca5dbe56227d1c68eb160c524d0446acfe7f7a4888619057522a9d03b9afe399ce5d9451f6ea764ff747d521c44cbbd21524a969bc9af6b'
+SPARKs::Application.config.secret_token = 'a9f11d85c9061ad6543c62a1c2e7fc57f4be4916fff91d96d1af4a3528e32686e8d149134362aeadc8aa9b970410a5f28ee7c67d844a82b703a0b005daea635d'
